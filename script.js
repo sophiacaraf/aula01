a89e3eec64a7aac5d398c2dca9379658c38bfd0c
sophiacaraf <sosopoinn@gmail.com>
2025-02-21 00:34:07 -0300

    script.js

--- script.js
+++ script.js
@@ -1,60 +1,56 @@
 let contatos = [];
 
-function adicionarContato(nome, telefone){
-    contatos.push ({nome, telefone});
-    atualizarTela ();
+function adicionarContato(nome, telefone) {
+    contatos.push({ nome, telefone });
+    atualizarTela();
 }
 
-function removerContato (index){
-    contatos.push({nome, telefone});
-    atualizarTela ();
+function removerContato(index) {
+    contatos.splice(index, 1); // Remove o contato correto
+    atualizarTela();
 }
 
-function editarContato (index){
-    let novoNome = prompt ("Digite o novo bnome: ", contatos [index].nome);
-    let novoTelefone = prompt ("digite o novo telefone: ", contatos [index].telefone);
+function editarContato(index) {
+    let novoNome = prompt("Digite o novo nome: ", contatos[index].nome);
+    let novoTelefone = prompt("Digite o novo telefone: ", contatos[index].telefone);
 
     if (novoNome && novoTelefone) {
-        contatos [index] = {nome: novoNome, telefone: novoTelefone};
-        atualizarTela ();
+        contatos[index] = { nome: novoNome, telefone: novoTelefone };
+        atualizarTela();
     }
-
 }
 
-function atualizarTela () {
+function atualizarTela() {
     let saida = "<h1>Lista de contatos</h1>";
 
-    if (contatos.lenght === 0){
-        let saida += "<p>Nenhum contato cadastrado.</p>";
+    if (contatos.length === 0) {
+        saida += "<p>Nenhum contato cadastrado.</p>";
     } else {
         saida += "<ul>";
         contatos.forEach((contato, index) => {
-            saida += '<li>${contato.nome} - ${contato.telefone}
+            saida += `<li>${contato.nome} - ${contato.telefone}
                     <button onclick="editarContato(${index})">
-                    editar</button>
+                    Editar</button>
                     <button onclick="removerContato(${index})">
-                    remover</button>
-                    </li>';
-
+                    Remover</button>
+                    </li>`;
         });
         saida += "</ul>";
     }
-     saida += '<button onclick="novoContato()"> Adicionar Contato </button>';
+    saida += '<button onclick="novoContato()">Adicionar Contato</button>';
 
-     document.body.innerHTMl = saida;
-
+    document.body.innerHTML = saida;
 }
 
-function novoContato(){
-    let nome = prompt ("Digite o nome do novo contato: ");
-    let telefone =prompt ("Digite o novo telefone: ");
+function novoContato() {
+    let nome = prompt("Digite o nome do novo contato: ");
+    let telefone = prompt("Digite o telefone do novo contato: ");
 
-    if (nome && telefone){
-        adicionarContato (nome, telefone);
-
+    if (nome && telefone) {
+        adicionarContato(nome, telefone);
     }
 }
 
-adicionarContato ("Maria", "99999-1111");
-adicionarContato ("Joao", "98888-2222");
-
+// Adicionando contatos iniciais
+adicionarContato("Maria", "99999-1111");
+adicionarContato("João", "98888-2222");
